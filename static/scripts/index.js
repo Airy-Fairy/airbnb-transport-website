5a2bc9db27a977bca2c7a85dc0ed392aa49d71f4
Gleb Ignatiev <glebignatieff@gmail.com>
2017-09-10 01:20:16 +0300

    Client side is ready to search :D

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -5,6 +5,7 @@ var current = 0;
  * Requests top 3 vehicles from server
  */
 $( document ).ready(function() {
+    $('.nav-search').hide();
     $.post(
         '/index',
         {
@@ -39,69 +40,8 @@ $('#get-more').click(function() {
  */
 function fill_preview(dataset) {
     for (var index in dataset) {
-        add_thumbnail(dataset[index]);
+        add_thumbnail(dataset[index], n_columns=3);
     }
-}
-
-/**
- * Adds one vehicle thumbnail to the preview
- * @param {json} data One vehicle data in json
- */
-function add_thumbnail(data) {
-    $grid = $('.grid');
-    var last_row;
-    var i;
-
-    // If grid is empty - add first row and 3 columns
-    if ($grid[0].children.length == 0) {
-        $grid.append('<div class="row"></div>');
-        last_row = $grid[0].children[$grid[0].children.length - 1];
-        for (i = 0; i < 3; i++) {
-            $(last_row).append('<div class="col-1-3"></div>');
-        }
-    }
-
-    // If current row is full - add new row and 3 columns
-    last_row = $grid[0].children[$grid[0].children.length - 1];
-    if (last_row.children[2].children.length != 0) {
-        $grid.append('<div class="row"></div>');
-        last_row = $grid[0].children[$grid[0].children.length - 1];
-        for (i = 0; i < 3; i++) {
-            $(last_row).append('<div class="col-1-3"></div>');
-        }
-    }
-
-    // Pick free cell
-    var last_col;
-    for (i = 0; i < 3; i++) {
-        if (last_row.children[i].children.length == 0) {
-            last_col = last_row.children[i];
-            break;
-        }
-    }
-
-    // Get json data
-    var price = data.price;
-    var show_name = data.show_name;
-    var rating = data.rating;
-    var desc = data.desc;
-    var reviews = data.reviews;
-
-    // Make new thumbnail
-    $thumbnail = $('.thumbnail.template').clone();
-    $thumbnail.removeClass('template');
-    $thumbnail.find('.label').text(price + ' ' + show_name);
-    $thumbnail.find('.short-info').text(desc);
-    $thumbnail.find('.reviews-count > .number').text(String(reviews));
-
-    var stars = $thumbnail.find('.star-rating').children();
-    for (i = 0; i < Math.round(rating); i++) {
-        $(stars[i]).removeClass('fa-star-o');
-        $(stars[i]).addClass('fa-star');
-    }
-
-    $(last_col).append($thumbnail);
-    $thumbnail.prop('hidden', false);
 }
 
 $('#huge-search').click(function() {
