444e05964ed9b8e33adacbf5a055153a589e4496
flask <rakcheev.d@gmail.com>
2017-09-12 19:18:26 -0700

    merge and some more funcs

--- wheels/static/scripts/new_review.js
+++ wheels/static/scripts/new_review.js
@@ -1,56 +1,61 @@
-// Shows 'Add review' and 'Cancel' buttons on focus
-$('.new-review .right-col .message').focusin(function() {
-    $('.new-review .add').show();
-});
+/**
+ *
+ * [NEW REVIEW THUMBNAIL]
+ *
+ */
 
-// Hides 'Add review' and 'Cancel' buttons on 'Cancel' click
-$('.new-review .add input#cancel-button').on('click', function() {
-    $('.new-review .add').hide();
-});
+ // Shows 'Add review' and 'Cancel' buttons on focus
+ $('.new-review .right-col .message').focusin(function() {
+     $('.new-review .add').show();
+ });
 
-// Adds new review on 'Add review' click
-// TODO: rating, image, username
-$('.new-review .add input#add-button').on('click', function(event) {
+ // Hides 'Add review' and 'Cancel' buttons on 'Cancel' click
+ $('.new-review .add input#cancel-button').on('click', function() {
+     $('.new-review .add').hide();
+ });
 
-    event.preventDefault();
+ // Adds new review on 'Add review' click
+ // TODO: rating, image, username
+ $('.new-review .add input#add-button').on('click', function(event) {
 
-    var chosenTransport = $('#choose-user-transport option:selected').text();
-    var message = $('#message').val();
-    var rating = $('.rating-choice .star-rating input:checked').val();
-    var image = '../../imgs/zoro.png';
-    var username = 'User';
+     event.preventDefault();
 
-    // 'required' check
-    if ($('#choose-user-transport option:selected').val() === "" ||
-        rating === undefined) {
-        alert('Fill the form (transport and rating at least)!');
-        return;
-    }
+     var chosenTransport = $('#choose-user-transport option:selected').text();
+     var message = $('#message').val();
+     var rating = $('.rating-choice .star-rating input:checked').val();
+     var image = '../../imgs/zoro.png';
+     var username = 'User';
 
-    // Get review template
-    var $newReview = $('.review.template').clone();
+     // 'required' check
+     if ($('#choose-user-transport option:selected').val() === "" ||
+         rating === undefined) {
+         alert('Fill the form (transport and rating at least)!');
+         return;
+     }
 
-    $newReview.removeClass('template');
-    console.log($newReview);
+     // Get review template
+     var $newReview = $('.review.template').clone();
 
-    // Fill the new review with entered data
-    $newReview.find('.image img').attr('src', image);
-    $newReview.find('.user-name').text(username);
-    $newReview.find('.transport').text(chosenTransport);
-    $newReview.find('.message').text(message);
-    $newReview.find('.post-time').text(getCurrentDate());
+     $newReview.removeClass('template');
 
-    var stars = $newReview.find('.star-rating').children();
-    for (var i = 0; i < rating; i++) {
-        $(stars[i]).removeClass('fa-star-o');
-        $(stars[i]).addClass('fa-star');
-    }
+     // Fill the new review with entered data
+     $newReview.find('.image img').attr('src', image);
+     $newReview.find('.user-name').text(username);
+     $newReview.find('.transport').text(chosenTransport);
+     $newReview.find('.message').text(message);
+     $newReview.find('.post-time').text(getCurrentDate());
 
-    // Add the new review
-    $('<li>' + $newReview[0].outerHTML + '</li>').insertAfter('ul.reviews-list>li:first-of-type');
+     var stars = $newReview.find('.star-rating').children();
+     for (var i = 0; i < rating; i++) {
+         $(stars[i]).removeClass('fa-star-o');
+         $(stars[i]).addClass('fa-star');
+     }
 
-    // Clear form
-    $('#choose-user-transport').val('');
-    $('#message').val('');
-    $('.rating-choice .star-rating input:checked').prop('checked', false);
-});
+     // Add the new review
+     $('<li>' + $newReview[0].outerHTML + '</li>').insertAfter('ul.reviews-list>li:first-of-type');
+
+     // Clear form
+     $('#choose-user-transport').val('');
+     $('#message').val('');
+     $('.rating-choice .star-rating input:checked').prop('checked', false);
+ });
